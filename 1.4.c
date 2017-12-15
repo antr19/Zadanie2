c209d3692d6eb2b3a8d9ff306def06dcccdc4df7
antr19 <32762114+antr19@users.noreply.github.com>
2017-12-15 12:40:20 +0300

    Update and rename 1.4 to 1.4.c

--- 1.4.c
+++ 1.4.c
@@ -13,31 +13,31 @@ int main()
 
     int n,k;
 
-    for(n=0;n<=(strlen(s));n++)
+    for(n=0;n<(strlen(s));n++)
     {
         k=0;
-        char a[1];
-
         while (i<strlen(s))
         {
             printf("pr2 ");
-            if(((s[i]="(")&&(s[i+1]=")"))||((s[i]="{")&&(s[i+1]="}")))
+            if(((s[i]='(')&&(s[i+1]=')'))||((s[i]='{')&&(s[i+1]='}')))
             {
+                if(strlen(s)==2)
+                    return 0;
                 printf("pr3 ");
                 int j;
+                printf("pr4 ");
                 for (j=0;j<=(strlen(s)-i-1);j++)
                 {
-                    printf("pr4 %i ",j);
+                    printf("pr5 ");
                     s[i+j]=s[i+j+2];
                 }
                 printf("pr5 ");
 
-                s.Remove(strlen-3,2);
+                s[strlen(s)-2]="\0";
 
                 k=1;
-                printf("Lol");
             }
-            printf("Lol");
+            printf("%s",s);
             i++;
         }
         if(k==0)
