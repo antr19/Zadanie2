0e4656e1d6e295c80b9cac9e7abeab60e86c9fb0
antr19 <32762114+antr19@users.noreply.github.com>
2017-12-18 13:03:07 +0300

    Update 1.4.c

--- 1.4.c
+++ 1.4.c
@@ -18,20 +18,20 @@ int main()
         k=0;
         while (i<strlen(s))
         {
-            printf("pr2 ");
+            //printf("pr2 ");
             if(((s[i]='(')&&(s[i+1]=')'))||((s[i]='{')&&(s[i+1]='}')))
             {
                 if(strlen(s)==2)
                     return 0;
-                printf("pr3 ");
+                //printf("pr3 ");
                 int j;
-                printf("pr4 ");
+                //printf("pr4 ");
                 for (j=0;j<=(strlen(s)-i-1);j++)
                 {
-                    printf("pr5 ");
+                    //printf("pr5 ");
                     s[i+j]=s[i+j+2];
                 }
-                printf("pr5 ");
+                //printf("pr5 ");
 
                 s[strlen(s)-2]="\0";
 
